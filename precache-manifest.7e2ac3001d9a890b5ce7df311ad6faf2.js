b06417061ad34193f598dbc4651d863aabab8f1c
Lucas Akira Uehara <Akira@akira.com>
2019-02-12 08:14:06 -0200

    Updates

---
+++ precache-manifest.7e2ac3001d9a890b5ce7df311ad6faf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbd22f8e9e55594c9afb",
+    "url": "/react-search-zipcode/static/css/main.31425b1a.chunk.css"
+  },
+  {
+    "revision": "fbd22f8e9e55594c9afb",
+    "url": "/react-search-zipcode/static/js/main.fbd22f8e.chunk.js"
+  },
+  {
+    "revision": "6a5806aae3b1c6881150",
+    "url": "/react-search-zipcode/static/css/1.10bde565.chunk.css"
+  },
+  {
+    "revision": "6a5806aae3b1c6881150",
+    "url": "/react-search-zipcode/static/js/1.6a5806aa.chunk.js"
+  },
+  {
+    "revision": "09d5556f98894c829ec6",
+    "url": "/react-search-zipcode/static/js/runtime~main.09d5556f.js"
+  },
+  {
+    "revision": "49b97415cbf78968864cbd1befbbc8f4",
+    "url": "/react-search-zipcode/index.html"
+  }
+];
